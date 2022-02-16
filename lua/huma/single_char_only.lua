d8c8e25fa6f103cd4ebdab474a25081359c37d45
ywxt <ywxtcwh@qq.com>
2022-02-16 14:12:29 +0800

    修復lua警告

--- lua/huma/single_char_only.lua
+++ lua/huma/single_char_only.lua
@@ -3,7 +3,7 @@
 --    https://github.com/rime/librime/issues/248#issuecomment-468924677
 
 local function filter(input, env)
-  b = env.engine.context:get_option("single_char_only")
+  local b = env.engine.context:get_option("single_char_only")
   for cand in input:iter() do
     if (not b or utf8.len(cand.text) == 1) then
       yield(cand)
