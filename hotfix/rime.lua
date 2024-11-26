b8e2670023934feb7edeb00a25269491f3c48a33
ywxt <ywxtcwh@qq.com>
2024-11-26 22:35:27 +0800

    add hotfix.

---
+++ hotfix/rime.lua
@@ -0,0 +1,7 @@
+spelling_processor = require('huma/spelling_processor')
+spelling = require('huma/spelling')
+single_char_only = require('huma/single_char_only')
+charset_filter = require('huma/charset_filter')
+postpone_fullcode = require('huma/postpone_fullcode')
+function_selector = require('huma/function_selector')
+unicode_code = require('huma/unicode_code')
